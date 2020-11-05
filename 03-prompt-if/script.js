494fad6c977bf5a20bf833c71260491e5b9e7ecf
Daems Aline <daems.aline90@gmail.com>
2020-11-05 10:47:54 +0100

    files 2

---
+++ 03-prompt-if/script.js
@@ -0,0 +1,28 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+  
+  
+   let Cake = prompt("Would you like some cake ?");
+
+  if (Cake == "Yes") {
+    alert ("Congrulations !");
+  }
+  
+  else  {
+    alert ("More cake for me then !");
+  }
+  
+
+  // your code here
+
+})();
